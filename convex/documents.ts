23ec8afd06af1d2f43b45ac96b112d0f0bdb5621
Mark Njoroge <marknjo7@gmail.com>
2024-02-26 14:07:22 +0300

    feat: implement create() document convex mutation method

--- convex/documents.ts
+++ convex/documents.ts
@@ -76,3 +76,29 @@ export const getSidebar = query({
     return documents;
   },
 });
+
+export const create = mutation({
+  args: {
+    title: v.string(),
+    parentDocument: v.optional(v.id('documents')),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error('Not authenticated');
+    }
+
+    const userId = identity.subject;
+
+    const document = await ctx.db.insert('documents', {
+      title: args.title,
+      parentDocument: args.parentDocument,
+      userId,
+      isArchived: false,
+      isPublished: false,
+    });
+
+    return document;
+  },
+});
