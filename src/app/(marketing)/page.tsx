7f07a0ae5da450db2416a4201ede9e1b61125c8f
Mark Njoroge <marknjo7@gmail.com>
2024-02-26 14:30:31 +0300

    fix: minor fixes to mrketing pages

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,11 +1,10 @@
-import { Button } from '@/components/ui/button';
 import { Heading } from './_components/heading';
 import { Heros } from './_components/heros';
 import { Footer } from './_components/footer';
 
 export default function MarketingPage() {
   return (
-    <div className='min-h-full flex flex-col'>
+    <div className='min-h-full flex flex-col dark:bg-[#1f1f1f]'>
       <div className='flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 p-10'>
         <Heading />
 
